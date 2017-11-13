9fb29015ee974cc9771bd56f589b7c02985d0dd6
irow0433 <31513820+irow0433@users.noreply.github.com>
2017-11-13 08:13:08 -0700

    finished basics for the Controller Class and Runner

--- src/gui/controller/GUIRunner.java
+++ src/gui/controller/GUIRunner.java
@@ -1,5 +1,12 @@
 package gui.controller;
 
-public class GUIRunner {
+public class GUIRunner {	
+	
+	public static void main(String[] args)
+	{
+		GUIAppController gui = new GUIAppController();
+		gui.start();
+	}
+	
 
 }
--- src/gui/view/GUIFrame.java
+++ src/gui/view/GUIFrame.java
@@ -1,5 +1,28 @@
 package gui.view;
 
-public class GUIFrame {
+import gui.controller.GUIAppController;
+import javax.swing.JFrame;
 
+//private is only for getters and setters	
+//"extends" is the word at inherits things
+public class GUIFrame extends JFrame
+{
+	private GUIAppController appController;
+	
+	public GUIFrame(GUIAppController appController)
+	{
+		//super calls the parent constructor
+		super();
+		this.appController  = appController;
+		
+		setupFrame();
+	}
+	//belongs to frame class and handles the components for the gui
+	private void setupFrame()
+	{
+		//requires all that is below in setupFrame
+		this.setTitle("The Window title");
+		this.setSize(500, 500);
+		this.setVisible(true);
+	}
 }
